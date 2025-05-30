c0eedc687c982b6277873c552cb66ed36dda9cce
VitorDiamanteNegro <168202201+VitorDiamanteNegro@users.noreply.github.com>
2025-05-30 11:21:36 -0300

    goodboy

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+let contador = 0;
+
+function incrementar() {
+  contador++;
+  document.getElementById('contador').innerText = contador;
+}
+
+function resetar() {
+  contador = 0;
+  document.getElementById('contador').innerText = contador;
+}
 
